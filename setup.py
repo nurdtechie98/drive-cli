9d82bafd3ec47ddd947d9664f8ac3cde419dc975
inishchith <inishchith@gmail.com>
2019-02-18 20:22:13 +0530

    Fix autopep8

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from drive_cli import __version__
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 
+
 def readme():
     try:
         with open('README.md') as f:
@@ -10,18 +11,18 @@ def readme():
         pass
 
 setup(
-    name ='drive_cli',
+    name='drive_cli',
     version='.'.join(str(i) for i in __version__),
-    description = 'CLI client for Google Drive',
+    description='CLI client for Google Drive',
     url='https://github.com/nurdtechie98/drive-cli',
-    author = 'Chirag Shetty',
-    author_email = 'nurdtechie98@gmail.com',
+    author='Chirag Shetty',
+    author_email='nurdtechie98@gmail.com',
 
-    long_description=readme(),  
+    long_description=readme(),
     long_description_content_type='text/markdown',
-    
-    packages = find_packages(),
-    include_package_data = True,
+
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'requests',
         'click',
@@ -35,17 +36,17 @@ setup(
         'pyfiglet'
     ],
     entry_points={
-		'console_scripts': [
-			'drive=drive_cli.dcli:cli'
-		]
-	},
+        'console_scripts': [
+            'drive=drive_cli.dcli:cli'
+        ]
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "Development Status :: 3 - Alpha",
         "Intended Audience :: End Users/Desktop",
-		"Natural Language :: English",
+        "Natural Language :: English",
         "Topic :: Utilities",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-     ],
+    ],
 )
