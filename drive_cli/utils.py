56a918b7708804aab92e83e0ba41c165f1ac26dc
Karolis Ramanauskas <karolisr@gmail.com>
2020-01-29 02:50:04 -0600

    Adds a missing closing parenthesis, which was causing a syntax error. (#87)

--- drive_cli/utils.py
+++ drive_cli/utils.py
@@ -375,10 +375,10 @@ def upload_file(name, path, pid):
                                           media_body=media,
                                           fields='id').execute()
     else:
-        CHUNK_SIZE_MB = int(os.getenv("CHUNK_SIZE_MB") or 1 # MB. You may want to increase the size to a higher speed if the network restrictions allow
+        CHUNK_SIZE_MB = int(os.getenv("CHUNK_SIZE_MB") or 1)  # MB. You may want to increase the size to a higher speed if the network restrictions allow
         media = MediaFileUpload(
             path, mimetype=file_mimeType,
-            chunksize=(1024 * 1024 * CHUNK_SIZE_MB) , resumable=True)
+            chunksize=(1024 * 1024 * CHUNK_SIZE_MB), resumable=True)
         status, new_file = None, None
         req = service.files().create(body=file_metadata,
                                      media_body=media,
